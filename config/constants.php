9b4da27a996d7056940ad9198e8ca9d1549842d6
GuilhermeBraz <gvcerverirabraz@gmail.com>
2024-04-12 17:47:43 -0300

    code

--- config/constants.php
+++ config/constants.php
@@ -4,11 +4,12 @@
 
 
     //Create Constants to Store Non Repeating Values
+    //change localhost to the vm ip address on the SITEURL
     define('SITEURL', 'http://localhost/food-order/');
     define('LOCALHOST', 'localhost');
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', '');
-    define('DB_NAME', 'food_db');
+    define('DB_NAME', 'food-order');
     
     $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error()); //Database Connection
     $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error()); //SElecting Database
