b2f4f5addaf267f31593c69675da9b9f7d7edc91
GuilhermeBraz <gvcerverirabraz@gmail.com>
2024-04-16 14:16:41 -0300

    change db name

--- config/constants.php
+++ config/constants.php
@@ -9,7 +9,7 @@
     define('LOCALHOST', 'localhost');
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', '');
-    define('DB_NAME', 'food-order');
+    define('DB_NAME', 'food_db'); //if database name is wrong app will not work (default was food-order)
     
     $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error()); //Database Connection
     $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error()); //SElecting Database
